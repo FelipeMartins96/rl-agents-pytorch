4c1f64fe8f918943b1879174858496253211c3ef
Felipe <felipemartins96@gmail.com>
2021-03-18 19:01:30 -0300

    🎨 add hyperparameters class

--- ddpg/ddpg.py
+++ ddpg/ddpg.py
@@ -6,7 +6,28 @@ import numpy as np
 from experience import *
 from noise import *
 import os
+from dataclasses import dataclass
 
+@dataclass
+class HyperParameters:
+    """Class containing all experiment hyperparameters"""
+    EXP_NAME: str
+    ENV_NAME: str
+    N_ROLLOUT_PROCESSES: int
+    LEARNING_RATE: float
+    REPLAY_SIZE: int # Maximum Replay Buffer Sizer
+    REPLAY_INITIAL: int # Minimum experience buffer size to start training
+    EXP_GRAD_RATIO: int  # Number of collected experiences for every grad step
+    SAVE_FREQUENCY: int  # Save checkpoint every _ grad_steps
+    BATCH_SIZE: int
+    GAMMA: float # Reward Decay
+    REWARD_STEPS: float  # For N-Steps Tracing
+    NOISE_SIGMA_INITIAL: float # Initial action noise sigma
+    NOISE_THETA: float
+    NOISE_SIGMA_DECAY: float  # Action noise sigma decay
+    NOISE_SIGMA_GRAD_STEPS: int  # Decay action noise every _ grad steps
+    N_OBS: int = 0
+    N_ACTS: int = 0
 
 def unpack_batch_ddpg(
     batch,
--- ddpg/train.py
+++ ddpg/train.py
@@ -16,22 +16,6 @@ from ddpg import *
 from networks import *
 from experience import *
 
-ENV = 'Pendulum-v0'
-ROLLOUT_PROCESSES_COUNT = 1
-LEARNING_RATE = 0.0001
-REPLAY_SIZE = 100000  # Maximum Replay Buffer Sizer
-REPLAY_INITIAL = 10000  # Minimum experience buffer size to start training
-BATCH_SIZE = 256
-GAMMA = 0.95  # Reward Decay
-REWARD_STEPS = 2  # For N-Steps Tracing
-SAVE_FREQUENCY = 1000  # Save checkpoint every _ grad_steps
-EXP_GRAD_RATIO = 10  # Number of collected experiences for every grad step
-NOISE_SIGMA_INITIAL = 1.0  # Initial action noise sigma
-NOISE_SIGMA_DECAY = 0.999  # Action noise sigma decay
-NOISE_SIGMA_GRAD_STEPS = 1000  # Decay action noise every _ grad steps
-NOISE_THETA = 0.15
-
-
 def get_env_specs(env_name):
     env = gym.make(env_name)
     return env.observation_space.shape[0], env.action_space.shape[0]
@@ -48,24 +32,45 @@ if __name__ == "__main__":
     args = parser.parse_args()
     device = "cuda" if args.cuda else "cpu"
 
-    path = os.path.join("saves", "ddpg", args.name)
+    # Input Experiment Hyperparameters
+    hp = HyperParameters(
+        EXP_NAME=args.name,
+        ENV_NAME='Pendulum-v0',
+        N_ROLLOUT_PROCESSES=1,
+        LEARNING_RATE=0.0001,
+        REPLAY_SIZE=100000,
+        REPLAY_INITIAL=10000,
+        EXP_GRAD_RATIO=10,
+        SAVE_FREQUENCY=1000,
+        BATCH_SIZE=256,
+        GAMMA=0.95,
+        REWARD_STEPS=2,
+        NOISE_SIGMA_INITIAL=1.0,
+        NOISE_THETA=0.15,
+        NOISE_SIGMA_DECAY=0.99,
+        NOISE_SIGMA_GRAD_STEPS=1000
+    )
+
+
+    path = os.path.join("saves", "ddpg", hp.EXP_NAME)
     checkpoint_path = os.path.join(path, "Checkpoints")
     current_time = datetime.datetime.now().strftime('%m-%d_%H-%M-%S')
-    tb_path = os.path.join('runs', ENV + '_' + current_time + '-' + args.name)
+    tb_path = os.path.join('runs', 
+                           hp.ENV_NAME + '_' + current_time + '-' + hp.EXP_NAME)
     os.makedirs(checkpoint_path, exist_ok=True)
 
-    n_obs, n_acts = get_env_specs(ENV)
+    hp.N_OBS, hp.N_ACTS = get_env_specs(hp.ENV_NAME)
 
-    pi = DDPGActor(n_obs, n_acts).to(device)
-    Q = DDPGCritic(n_obs, n_acts).to(device)
+    pi = DDPGActor(hp.N_OBS, hp.N_ACTS).to(device)
+    Q = DDPGCritic(hp.N_OBS, hp.N_ACTS).to(device)
 
     # Playing
     pi.share_memory()
-    exp_queue = mp.Queue(maxsize=BATCH_SIZE)
+    exp_queue = mp.Queue(maxsize=hp.BATCH_SIZE)
     finish_event = mp.Event()
-    noise_sigma_m = mp.Value('f', NOISE_SIGMA_INITIAL)
+    noise_sigma_m = mp.Value('f', hp.NOISE_SIGMA_INITIAL)
     data_proc_list = []
-    for _ in range(ROLLOUT_PROCESSES_COUNT):
+    for _ in range(hp.N_ROLLOUT_PROCESSES):
         data_proc = mp.Process(
             target=data_func,
             args=(
@@ -73,11 +78,11 @@ if __name__ == "__main__":
                 device,
                 exp_queue,
                 finish_event,
-                ENV,
-                GAMMA,
-                REWARD_STEPS,
+                hp.ENV_NAME,
+                hp.GAMMA,
+                hp.REWARD_STEPS,
                 noise_sigma_m,
-                NOISE_THETA
+                hp.NOISE_THETA
             )
         )
         data_proc.start()
@@ -87,9 +92,9 @@ if __name__ == "__main__":
     writer = SummaryWriter(tb_path)
     tgt_pi = TargetActor(pi)
     tgt_Q = TargetCritic(Q)
-    pi_opt = optim.Adam(pi.parameters(), lr=LEARNING_RATE)
-    Q_opt = optim.Adam(Q.parameters(), lr=LEARNING_RATE)
-    buffer = ExperienceReplayBuffer(buffer_size=REPLAY_SIZE)
+    pi_opt = optim.Adam(pi.parameters(), lr=hp.LEARNING_RATE)
+    Q_opt = optim.Adam(Q.parameters(), lr=hp.LEARNING_RATE)
+    buffer = ExperienceReplayBuffer(buffer_size=hp.REPLAY_SIZE)
     n_grads = 0
     n_samples = 0
     n_episodes = 0
@@ -101,7 +106,7 @@ if __name__ == "__main__":
             st_time = time.perf_counter()
             # Collect EXP_GRAD_RATIO sample for each grad step
             new_samples = 0
-            while new_samples < EXP_GRAD_RATIO:
+            while new_samples < hp.EXP_GRAD_RATIO:
                 exp = exp_queue.get()
                 if exp is None:
                     raise Exception  # got None value in queue
@@ -123,15 +128,15 @@ if __name__ == "__main__":
             n_samples += new_samples
             sample_time = time.perf_counter()
 
-            if len(buffer) < REPLAY_SIZE:
+            if len(buffer) < hp.REPLAY_SIZE:
                 # Track buffer filling speed
                 writer.add_scalar("buffer/len", len(buffer), n_samples)
                 # Only start training after buffer is larger than initial value
-                if len(buffer) < REPLAY_INITIAL:
+                if len(buffer) < hp.REPLAY_INITIAL:
                     continue
 
             # Sample a batch and load it as a tensor on device
-            batch = buffer.sample(BATCH_SIZE)
+            batch = buffer.sample(hp.BATCH_SIZE)
             S_v, A_v, r_v, dones, S_next_v = unpack_batch_ddpg(batch, device)
 
             # train critic
@@ -141,7 +146,7 @@ if __name__ == "__main__":
             Q_next_v = tgt_Q(S_next_v, A_next_v)  # Bootstrap Q_next
             Q_next_v[dones] = 0.0  # No bootstrap if transition is terminal
             # Calculate a reference Q value using the bootstrap Q
-            Q_ref_v = r_v.unsqueeze(dim=-1) + Q_next_v * (GAMMA**REWARD_STEPS)
+            Q_ref_v = r_v.unsqueeze(dim=-1) + Q_next_v * (hp.GAMMA**hp.REWARD_STEPS)
             Q_loss_v = F.mse_loss(Q_v, Q_ref_v.detach())
             Q_loss_v.backward()
             Q_opt.step()
@@ -174,15 +179,15 @@ if __name__ == "__main__":
                     global_step=n_grads
                 )
             
-            if n_grads % NOISE_SIGMA_GRAD_STEPS == 0:
+            if n_grads % hp.NOISE_SIGMA_GRAD_STEPS == 0:
                 # This syntax is needed to be process-safe
                 # The noise sigma value is accessed by the playing processes
                 with noise_sigma_m.get_lock():
-                    noise_sigma_m.value *= NOISE_SIGMA_DECAY
+                    noise_sigma_m.value *= hp.NOISE_SIGMA_DECAY
 
-            if n_grads % SAVE_FREQUENCY == 0:
+            if n_grads % hp.SAVE_FREQUENCY == 0:
                 save_checkpoint(
-                    experiment=args.name,
+                    experiment=hp.EXP_NAME,
                     agent="ddpg_async",
                     pi=pi,
                     Q=Q,
