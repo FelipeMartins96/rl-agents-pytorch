ba50f1257145de97373abc3eb6d6ade095826489
goncamateus <mgm4@cin.ufpe.br>
2021-03-31 23:16:25 -0300

    mixed the names

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,5 +1,5 @@
-python train_sac.py --cuda -n SAC_VSS-v0_00 &&
-python train_sac.py --cuda -n SAC_VSS-v0_01 &&
-python train_sac.py --cuda -n SAC_VSS-v0_02 &&
-python train_sac.py --cuda -n SAC_VSS-v0_03 &&
-python train_sac.py --cuda -n SAC_VSS-v0_04
+python train_ddpg.py --cuda -n DDPG_VSS-v0_00 &&
+python train_ddpg.py --cuda -n DDPG_VSS-v0_01 &&
+python train_ddpg.py --cuda -n DDPG_VSS-v0_02 &&
+python train_ddpg.py --cuda -n DDPG_VSS-v0_03 &&
+python train_ddpg.py --cuda -n DDPG_VSS-v0_04
