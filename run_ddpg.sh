b8c174d2505b25d6f2475b7545b7b762306b9f9d
goncamateus <mgm4@cin.ufpe.br>
2021-04-09 14:59:35 -0300

    fix shell scripts

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0 1 2 3 4; do
-  python train_ddpg.py --cuda -n "DDPG_VSS-v0_0$i"
+  python train_ddpg.py --cuda -n "DDPG_SSLGoToBallIR-v1_0$i" -e SSLGoToBallIR-v1
 done
--- run_sac.sh
+++ run_sac.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0 1 2 3 4; do
-  python train_sac.py --cuda -n "SAC_SSLGoToBallIR-v1_0$i"
+  python train_sac.py --cuda -n "SAC_SSLGoToBallIR-v1_0$i" -e SSLGoToBallIR-v1
 done
