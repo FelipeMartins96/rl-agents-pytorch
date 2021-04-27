366fe3a7c46063a8967ea29c86b9a8d927dda247
Felipe <felipemartins96@gmail.com>
2021-04-27 18:15:24 -0300

    change script order

--- run_static.sh
+++ run_static.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 0 1 2 3 4; do
-  python train_ddpg.py --cuda -n "DDPG_SSLStaticDefenders-v0_0$i" -e SSLStaticDefenders-v0
   python train_sac.py --cuda -n "SAC_SSLStaticDefenders-v0_0$i" -e SSLStaticDefenders-v0
+  python train_ddpg.py --cuda -n "DDPG_SSLStaticDefenders-v0_0$i" -e SSLStaticDefenders-v0
 done
