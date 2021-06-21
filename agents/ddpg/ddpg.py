8373747e666d3f0f08dc904902a4b5262d1f311d
goncamateus <mgm4@cin.ufpe.br>
2021-06-21 17:23:08 -0300

    fixes critic loss

--- agents/ddpg/ddpg.py
+++ agents/ddpg/ddpg.py
@@ -299,8 +299,8 @@ class DDPGStratRew(DDPG):
         next_state_action = self.tgt_pi(next_state_batch)
         qf_next_target = self.tgt_Q(next_state_batch, next_state_action)
         qf_next_target[mask_batch] = 0.0
-        next_q_value = reward_batch * self.rew_alpha + self.gamma * qf_next_target 
-        qf = self.Q(state_batch, action_batch)
+        next_q_value = (reward_batch  + self.gamma * qf_next_target )* self.rew_alpha
+        qf = self.Q(state_batch, action_batch)* self.rew_alpha
         Q_loss = F.mse_loss(qf, next_q_value.detach())
 
         pi = self.pi(state_batch)
