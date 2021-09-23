f45c6cf3cf7e07e35a17e987c2b94b81d7f4ac17
Felipe Martins <felipemartins96@gmail.com>
2021-09-22 21:36:46 -0300

    refactor(DDPG): Add tqdm and changes to log frequency
    gif is a bit buggy on wandb

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for i in 0 1 2 3 4; do
-  python train_ddpg.py --cuda -n "DDPG_SSLGoToBall-v0_0$i" -e SSLGoToBall-v0
+for i in 0; do
+  python train_ddpg.py --cuda -n "DDPG_VSS5V5_0$i" -e VSS5V5-v0
 done
--- train_ddpg.py
+++ train_ddpg.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import deque
 import copy
 import dataclasses
 import datetime
@@ -11,6 +12,7 @@ import rsoccer_gym
 import torch.multiprocessing as mp
 import torch.nn.functional as F
 import torch.optim as optim
+from tqdm import tqdm
 
 import wandb
 from agents.ddpg import (DDPGHP, DDPGActor, DDPGCritic, TargetActor,
@@ -39,7 +41,7 @@ if __name__ == "__main__":
         EXP_NAME=args.name,
         DEVICE=device,
         ENV_NAME=args.env,
-        N_ROLLOUT_PROCESSES=2,
+        N_ROLLOUT_PROCESSES=3,
         LEARNING_RATE=0.0001,
         EXP_GRAD_RATIO=10,
         BATCH_SIZE=256,
@@ -53,10 +55,10 @@ if __name__ == "__main__":
         REPLAY_SIZE=5000000,
         REPLAY_INITIAL=100000,
         SAVE_FREQUENCY=100000,
-        GIF_FREQUENCY=100000,
+        GIF_FREQUENCY=25000,
         TOTAL_GRAD_STEPS=2000000
     )
-    wandb.init(project='RoboCIn-RL', name=hp.EXP_NAME,  entity='robocin', config=hp.to_dict())
+    wandb.init(project='5v5', name=hp.EXP_NAME,  entity='robocin', config=hp.to_dict())
     current_time = datetime.datetime.now().strftime('%b-%d_%H-%M-%S')
     tb_path = os.path.join('runs', current_time + '_'
                            + hp.ENV_NAME + '_' + hp.EXP_NAME)
@@ -102,119 +104,129 @@ if __name__ == "__main__":
     n_episodes = 0
     best_reward = None
     last_gif = None
-
+    wandb_gif_queue = deque()
     try:
-        while n_grads < hp.TOTAL_GRAD_STEPS:
-            metrics = {}
+        with tqdm(total=hp.TOTAL_GRAD_STEPS) as pbar:
             ep_infos = list()
-            st_time = time.perf_counter()
-            # Collect EXP_GRAD_RATIO sample for each grad step
-            new_samples = 0
-            while new_samples < hp.EXP_GRAD_RATIO:
-                exp = exp_queue.get()
-                if exp is None:
-                    raise Exception  # got None value in queue
-                safe_exp = copy.deepcopy(exp)
-                del(exp)
+            while n_grads < hp.TOTAL_GRAD_STEPS:
+                metrics = {}
+                st_time = time.perf_counter()
+                # Collect EXP_GRAD_RATIO sample for each grad step
+                new_samples = 0
+                while new_samples < hp.EXP_GRAD_RATIO:
+                    exp = exp_queue.get()
+                    if exp is None:
+                        raise Exception  # got None value in queue
+                    safe_exp = copy.deepcopy(exp)
+                    del(exp)
 
-                # Dict is returned with end of episode info
-                if isinstance(safe_exp, dict):
-                    logs = {"ep_info/"+key: value for key,
-                            value in safe_exp.items() if 'truncated' not in key}
-                    ep_infos.append(logs)
-                    n_episodes += 1
-                else:
-                    buffer.add(
-                    obs=safe_exp.state,
-                    next_obs=safe_exp.last_state if safe_exp.last_state is not None else safe_exp.state,
-                    action=safe_exp.action,
-                    reward=safe_exp.reward,
-                    done=False if safe_exp.last_state is not None else True
+                    # Dict is returned with end of episode info
+                    if isinstance(safe_exp, dict):
+                        logs = {"ep_info/"+key: value for key,
+                                value in safe_exp.items() if 'truncated' not in key}
+                        ep_infos.append(logs)
+                        n_episodes += 1
+                    else:
+                        buffer.add(
+                        obs=safe_exp.state,
+                        next_obs=safe_exp.last_state if safe_exp.last_state is not None else safe_exp.state,
+                        action=safe_exp.action,
+                        reward=safe_exp.reward,
+                        done=False if safe_exp.last_state is not None else True
+                        )
+                        new_samples += 1
+                n_samples += new_samples
+                sample_time = time.perf_counter()
+
+
+                # Only start training after buffer is larger than initial value
+                if buffer.size() < hp.REPLAY_INITIAL:
+                    continue
+
+                # Sample a batch and load it as a tensor on device
+                batch = buffer.sample(hp.BATCH_SIZE)
+                S_v = batch.observations
+                A_v = batch.actions
+                r_v = batch.rewards
+                dones = batch.dones
+                S_next_v = batch.next_observations
+
+                # train critic
+                Q_opt.zero_grad()
+                Q_v = Q(S_v, A_v)  # expected Q for S,A
+                A_next_v = tgt_pi(S_next_v)  # Get an Bootstrap Action for S_next
+                Q_next_v = tgt_Q(S_next_v, A_next_v)  # Bootstrap Q_next
+                Q_next_v[dones == 1.] = 0.0  # No bootstrap if transition is terminal
+                # Calculate a reference Q value using the bootstrap Q
+                Q_ref_v = r_v + Q_next_v * (hp.GAMMA**hp.REWARD_STEPS)
+                Q_loss_v = F.mse_loss(Q_v, Q_ref_v.detach())
+                Q_loss_v.backward()
+                Q_opt.step()
+                metrics["train/loss_Q"] = Q_loss_v.cpu().detach().numpy()
+
+                # train actor - Maximize Q value received over every S
+                pi_opt.zero_grad()
+                A_cur_v = pi(S_v)
+                pi_loss_v = -Q(S_v, A_cur_v)
+                pi_loss_v = pi_loss_v.mean()
+                pi_loss_v.backward()
+                pi_opt.step()
+                metrics["train/loss_pi"] = pi_loss_v.cpu().detach().numpy()
+
+                # Sync target networks
+                tgt_pi.sync(alpha=1 - 1e-3)
+                tgt_Q.sync(alpha=1 - 1e-3)
+
+                n_grads += 1
+                grad_time = time.perf_counter()
+                if n_grads % 10 == 0:
+                    metrics['speed/samples'] = new_samples/(sample_time - st_time)
+                    metrics['speed/grad'] = 1/(grad_time - sample_time)
+                    metrics['speed/total'] = 1/(grad_time - st_time)
+                    metrics['counters/samples'] = n_samples
+                    metrics['counters/grads'] = n_grads
+                    metrics['counters/episodes'] = n_episodes
+                    metrics["counters/buffer_len"] = buffer.size()
+
+                    if ep_infos:
+                        for key in ep_infos[0].keys():
+                            metrics[key] = np.mean([info[key] for info in ep_infos])
+                    ep_infos = list()
+                    
+                    pbar.update(10)
+
+                    if len(wandb_gif_queue):
+                        if os.path.isfile(wandb_gif_queue[0]):
+                            wandb.log({"GIF": wandb.Video(wandb_gif_queue.popleft(), f"{n_grads}", fps=25, format="gif")}, commit=False)
+
+                    # Log metrics
+                    wandb.log(metrics)
+
+                if hp.NOISE_SIGMA_DECAY and sigma_m.value > hp.NOISE_SIGMA_MIN \
+                    and n_grads % hp.NOISE_SIGMA_GRAD_STEPS == 0:
+                    # This syntax is needed to be process-safe
+                    # The noise sigma value is accessed by the playing processes
+                    with sigma_m.get_lock():
+                        sigma_m.value *= hp.NOISE_SIGMA_DECAY
+
+                if hp.SAVE_FREQUENCY and n_grads % hp.SAVE_FREQUENCY == 0:
+                    save_checkpoint(
+                        hp=hp,
+                        metrics={
+                            'noise_sigma': sigma_m.value,
+                            'n_samples': n_samples,
+                            'n_episodes': n_episodes,   
+                            'n_grads': n_grads,
+                        },
+                        pi=pi,
+                        Q=Q,
+                        pi_opt=pi_opt,
+                        Q_opt=Q_opt
                     )
-                    new_samples += 1
-            n_samples += new_samples
-            sample_time = time.perf_counter()
 
-
-            # Only start training after buffer is larger than initial value
-            if buffer.size() < hp.REPLAY_INITIAL:
-                continue
-
-            # Sample a batch and load it as a tensor on device
-            batch = buffer.sample(hp.BATCH_SIZE)
-            S_v = batch.observations
-            A_v = batch.actions
-            r_v = batch.rewards
-            dones = batch.dones
-            S_next_v = batch.next_observations
-
-            # train critic
-            Q_opt.zero_grad()
-            Q_v = Q(S_v, A_v)  # expected Q for S,A
-            A_next_v = tgt_pi(S_next_v)  # Get an Bootstrap Action for S_next
-            Q_next_v = tgt_Q(S_next_v, A_next_v)  # Bootstrap Q_next
-            Q_next_v[dones == 1.] = 0.0  # No bootstrap if transition is terminal
-            # Calculate a reference Q value using the bootstrap Q
-            Q_ref_v = r_v + Q_next_v * (hp.GAMMA**hp.REWARD_STEPS)
-            Q_loss_v = F.mse_loss(Q_v, Q_ref_v.detach())
-            Q_loss_v.backward()
-            Q_opt.step()
-            metrics["train/loss_Q"] = Q_loss_v.cpu().detach().numpy()
-
-            # train actor - Maximize Q value received over every S
-            pi_opt.zero_grad()
-            A_cur_v = pi(S_v)
-            pi_loss_v = -Q(S_v, A_cur_v)
-            pi_loss_v = pi_loss_v.mean()
-            pi_loss_v.backward()
-            pi_opt.step()
-            metrics["train/loss_pi"] = pi_loss_v.cpu().detach().numpy()
-
-            # Sync target networks
-            tgt_pi.sync(alpha=1 - 1e-3)
-            tgt_Q.sync(alpha=1 - 1e-3)
-
-            n_grads += 1
-            grad_time = time.perf_counter()
-            metrics['speed/samples'] = new_samples/(sample_time - st_time)
-            metrics['speed/grad'] = 1/(grad_time - sample_time)
-            metrics['speed/total'] = 1/(grad_time - st_time)
-            metrics['counters/samples'] = n_samples
-            metrics['counters/grads'] = n_grads
-            metrics['counters/episodes'] = n_episodes
-            metrics["counters/buffer_len"] = buffer.size()
-
-            if ep_infos:
-                for key in ep_infos[0].keys():
-                    metrics[key] = np.mean([info[key] for info in ep_infos])
-
-            # Log metrics
-            wandb.log(metrics)
-
-            if hp.NOISE_SIGMA_DECAY and sigma_m.value > hp.NOISE_SIGMA_MIN \
-                and n_grads % hp.NOISE_SIGMA_GRAD_STEPS == 0:
-                # This syntax is needed to be process-safe
-                # The noise sigma value is accessed by the playing processes
-                with sigma_m.get_lock():
-                    sigma_m.value *= hp.NOISE_SIGMA_DECAY
-
-            if hp.SAVE_FREQUENCY and n_grads % hp.SAVE_FREQUENCY == 0:
-                save_checkpoint(
-                    hp=hp,
-                    metrics={
-                        'noise_sigma': sigma_m.value,
-                        'n_samples': n_samples,
-                        'n_episodes': n_episodes,   
-                        'n_grads': n_grads,
-                    },
-                    pi=pi,
-                    Q=Q,
-                    pi_opt=pi_opt,
-                    Q_opt=Q_opt
-                )
-
-            if hp.GIF_FREQUENCY and n_grads % hp.GIF_FREQUENCY == 0:
-                gif_req_m.value = n_grads
+                if hp.GIF_FREQUENCY and n_grads % hp.GIF_FREQUENCY == 0:
+                    gif_req_m.value = n_grads
+                    wandb_gif_queue.append(os.path.join(hp.GIF_PATH, f"{n_grads:09d}.gif"))
 
     except KeyboardInterrupt:
         print("...Finishing...")
