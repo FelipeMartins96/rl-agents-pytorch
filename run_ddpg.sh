61124f431ada0ceeb0e4ca1975b3d5c002cb1348
Felipe Martins <felipemartins96@gmail.com>
2021-10-12 02:52:42 -0300

    new run

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in 0; do
-  python train_ddpg.py --cuda -n "vss_BRW_rsim_v1" -e VSS_BRW_rsim-v1
-  python train_ddpg.py --cuda -n "vss_BRW_rsim_v2" -e VSS_BRW_rsim-v2
+  python train_ddpg.py --cuda -n "vss_BRW_rsim_v3_halfgrad" -e VSS_BRW_rsim-v3
+  python train_ddpg.py --cuda -n "vss_BRW_rsim_v4_halfmove" -e VSS_BRW_rsim-v4
 done
