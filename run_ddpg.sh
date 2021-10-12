7a5b60bf23094a9d6a9630b808b3eaca77d9436d
Felipe Martins <felipemartins96@gmail.com>
2021-10-12 20:51:45 -0300

    5v5_train
    5v5

--- run_ddpg.sh
+++ run_ddpg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 for i in 0; do
-  python train_ddpg.py --cuda -n "vss_BRW_rsim_v3_halfgrad" -e VSS_BRW_rsim-v3
-  python train_ddpg.py --cuda -n "vss_BRW_rsim_v4_halfmove" -e VSS_BRW_rsim-v4
+  python train_ddpg.py --cuda -n "VSS_A_rsimTuneTorque" -e VSS_A_rsim-v0
 done
