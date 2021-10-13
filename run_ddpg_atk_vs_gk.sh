ca541f10230696200a5972a972c348017965d244
matheusalb <mvca@cin.ufpe.br>
2021-10-13 14:14:07 -0300

    update run

--- run_ddpg_atk_vs_gk.sh
+++ run_ddpg_atk_vs_gk.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for i in 0 1 2 3 4; do
-  python train_ddpg_atk_vs_gk.py --cuda -n "DDPG_ATKVSGK-v0_0$i" -e VSSATKVSGK-v0
+  python train_ddpg_atk_vs_gk.py --cuda -n "DDPG_ATKVSGK-v0_0$i" -e VSSATKVSGK-v0 --checkpoint models/atk.pth --checkpoint_gk models/gk_ft2.pth
 done
